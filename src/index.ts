d33e83232b7211224211854b417a0f50f707486f
HaXGamiG <46896494+HaXGamiG@users.noreply.github.com>
2023-10-14 19:21:33 +0200

    feat: add method in queryrow

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import KnexInstance from '@lib/classes/KnexInstance'
 import QueryRow from '@lib/classes/QueryRow'
-import { WhereObject } from '@lib/types/QueryRow'
+import { WhereObject, FindAllOptions, OrderBy } from '@lib/types/QueryRow'
 
 export * from '@lib/decorators'
-export { KnexInstance, QueryRow, WhereObject }
+export { KnexInstance, QueryRow, WhereObject, FindAllOptions, OrderBy }
